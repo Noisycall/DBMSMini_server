d87adc788072d7cc30661455ba2e24c7041cd709
Noisycall <rajekunal4@gmail.com>
2020-09-05 12:41:03 +0530

    Set some env vars

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
-var app = express();
+const usersRouter = require("./routes/users");
 
+const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,5 +14,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/users", usersRouter);
+console.log(process.env.NODE_ENV);
 
 app.listen(8080);
